be3f2f7e686ab17269fdd789306a65d24811ca76
Ndagijimana <ndagijim22@egr-linux-6331.campus.up.edu>
2019-12-03 10:42:56 -0800

    Gave robots new eyes . Added my name to author list.

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw, Nathan Schmedake
+ * @author Tanya L. Crenshaw, Nathan Schmedake, Moses N.
  * @since August 2013
  *
  */
@@ -55,7 +55,7 @@ void dalekPrintAscii(void)
   printf("\n\n"
 	 "I am Dalek, from Dr. Who\n"
 	 "%s      <----->      \n" 
-         "%s     <  (0)  >     \n"        
+ "%s     <  (0)  >     \n"        
          "%s     |       |     \n"
          "%s    < ------- >    \n"
          "%s    o         o    \n"
